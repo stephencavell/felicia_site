bd665357b744d1f4c83cba36e4312a9b16004ddb
Stephen Cavell <cavell.stpehen@gmail.com>
2018-02-25 12:07:14 -0800

    just a commit for this branch

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,2 +1,34 @@
 //gallery.js
 
+$(document).ready(function () {
+
+	$("img").on("click", function() {
+		openGallery(this);
+	});
+
+	$(document).on("click", ".button-close", function() {
+		// closeGallery();
+		$('.gallery').hide();
+		console.log("button was clicked!!!!");
+	});
+
+	function openGallery(e) {
+		// $(".gallery").css('display', 'flex');
+		$('.gallery').show();
+		$(".gallery-photo").attr('src', e.src);
+		console.log('open gallery!!!!');
+	}
+
+	function closeGallery() {
+		$(".gallery").css('display', 'none');
+		console.log('close gallery');
+	}
+
+	function next() {
+
+	}
+
+	function previous() {
+
+	}
+});
